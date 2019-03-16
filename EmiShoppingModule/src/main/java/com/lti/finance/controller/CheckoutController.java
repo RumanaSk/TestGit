3b69713a726a649d64b8fb6e47faa655c272229d
RumanaSk <36967693+RumanaSk@users.noreply.github.com>
2019-03-16 19:49:27 +0530

    Add files via upload

---
+++ EmiShoppingModule/src/main/java/com/lti/finance/controller/CheckoutController.java
@@ -0,0 +1,30 @@
+package com.lti.finance.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "checkout")
+public class CheckoutController {
+
+	@RequestMapping(value = "payment", method = RequestMethod.GET)
+public String paymentgateway() {
+		return "redirect:/paymentgateway.jsp";
+	}
+	@RequestMapping(value = "emi", method = RequestMethod.GET)
+	
+	public String checkEmiOptions(@RequestParam("sum") String cartTotal,HttpSession session) {
+			
+		session.setAttribute("cartTotal",cartTotal);
+
+		return "redirect:/checkout.jsp";
+	
+		}
+
+	
+	
+}
---
+++ EmiShoppingModule/src/main/webapp/showproducts.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Thankyou products successfully added!</h1>
+<form action="controller/product/list" method="get">
+<input type="submit" value="show products">
+
+<a href="productUpload.jsp">Continue
+		Adding Products</a> 
+</body>
+</html>
